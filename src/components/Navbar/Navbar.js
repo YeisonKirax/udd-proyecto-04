6a3cc3f9009843fefa9fbe53bbc8a34ead241bbc
Yeison Fernandez <yeison.fernandez.14@sansano.usm.cl>
2022-11-23 23:39:31 -0300

    barra de navegación mejorada con bootstrap

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,46 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './navbar.css';
-export default function Navbar() {
-  const [ isNavExpanded, setIsNavExpanded ] = useState( false )
-
+export default function NavbarCustom() {
   return (
-    <nav className="navigation">
-      <a href="/" className="brand-name">
-        Restaurant
-      </a>
-      <button className="hamburger" onClick={ () => {
-        setIsNavExpanded( !isNavExpanded );
-      } }>
-        {/* icon from heroicons.com */ }
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="white"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
-      <div
-        className={
-          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-        }>
-        <ul>
-          <li>
-            <a href="/about-us">Nosotros</a>
-          </li>
-          <li>
-            <a href="/menu">Menú</a>
-          </li>
-          <li>
-            <a href="/reservations">Reservas</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+    <Navbar bg="dark" variant='dark' expand="lg">
+      <Container>
+        <Navbar.Brand href="/about-us">
+          <img src='https://react-bootstrap.github.io/logo.svg' width="30" height="30" className="d-inline-block align-top"
+            alt="React Bootstrap logo"></img>
+          Restaurant
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className="me-auto">
+            <Nav.Link href='/about-us'>Nosotros</Nav.Link>
+            <Nav.Link href='/menu'>Menú</Nav.Link>
+            <Nav.Link href='/reservations'>Reservaciones</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+
+    </Navbar >
+  )
 }
