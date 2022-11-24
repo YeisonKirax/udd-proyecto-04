1780eebd78120c822ab9eadadefb6b35c02d6453
Yeison Fernandez <yeison.fernandez.14@sansano.usm.cl>
2022-11-24 01:45:34 -0300

    actualizada vista de reservas

--- src/components/Reservas/Reservas.js
+++ src/components/Reservas/Reservas.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Tab, Tabs } from 'react-bootstrap';
+import FormularioReservacion from './FormularioReservacion/FormularioReservacion';
+import ListaReservaciones from './ListaReservaciones/ListaReservaciones';
+
 import './reservas.css';
 
 export default function Reservas() {
 
   return (
-    <h1>Sección Reservas</h1>
+    <Tabs defaultActiveKey={ "reservaciones" } id="reservaciones-tabs" fill={ true }>
+      <Tab eventKey={ "reservaciones" } title="Reservaciones">
+        <ListaReservaciones></ListaReservaciones>
+      </Tab>
+      <Tab title="Reservar hora" eventKey={ "reservar" }>
+        <FormularioReservacion></FormularioReservacion>
+      </Tab>
+    </Tabs>
   );
 }
